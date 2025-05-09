a33ccb4e8f29b6c58999559a7a83a8f4c703dd80
Droge <stephanie.droge@monster.com>
2025-05-08 23:21:15 -0700

    Initial MIRA Cloud deployment

---
+++ Launch_MIRA_Responsive_Final_With_Border.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\Users\stedroge\OneDrive - Monster_AD\Desktop\MIRA_App"
+C:\Users\stedroge\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run mira_streamlit_full_final_responsive.py
+pause
